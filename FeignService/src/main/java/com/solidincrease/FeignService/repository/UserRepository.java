459a5ed149c5adedf88d8e80cd3eae94df5a3777
diako83 <ismaildiako@gmail.com>
2023-06-28 15:53:50 +0200

    first

--- FeignService/src/main/java/com/solidincrease/FeignService/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.solidincrease.FeignService.repository;
-
-import com.soldincrease.FeignServices.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users,String> {
-    boolean existsUsersByEmail(String email);
-}
